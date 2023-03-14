3d6d3fa0739d2bc76a0550f6947cd6fef976ac22
vakh13 <127894409+vakh13@users.noreply.github.com>
2023-03-15 00:13:06 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+// let num = 100;
+
+// if (num < 50){
+//     console.log("naklebia 50-ze")
+// }
+// else if (num > 20) {
+//     console.log("metia 20-ze")
+// }
+// else {
+//     console.log("shecdoma")
+// }
+
+
+// 2.
+
+// let name = "mariam";
+
+// let statement = (name == "mariam") ? true : false;
+
+// console.log(statement);
+
+
+// 3.
+
+// let firstName = "mariam"
+
+// switch (firstName) {
+//     case "mariam":
+//         console.log(true);
+//         break;
+//     default:
+//         console.log(false)
+// }
